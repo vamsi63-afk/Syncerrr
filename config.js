f1daa5bafc97a71bd0aa14975cc60438ecab3dc2
vamsi63-afk <vamsi14du@gmail.com>
2026-05-09 23:35:15 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // config.js
 module.exports = {
   token: process.env.DISCORD_TOKEN,
-  prefix: require("./prefix.json").prefix || "."
+prefix: require("./prefix.json").prefix || ".",
   enablePrefix: true,
   supportServer: "https://discord.gg/9MVAPpfs8D",
 
